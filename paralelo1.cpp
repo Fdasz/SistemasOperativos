8604c7a60a7baf2f2cb307450233d2de1a43b04e
Sebastian Gonzalez Peña <147022362+mikufan82@users.noreply.github.com>
2023-12-03 18:06:52 -0300

    Se agregan comentarios

--- paralelo1.cpp
+++ paralelo1.cpp
@@ -7,15 +7,17 @@
 using namespace cv;
 using namespace std;
 
+//se usa funcion para utilizar metodo de luminosidad en la imagen
 void convertToGrayscaleSection(Mat& image, int start_row, int end_row) {
 	for (int i = start_row; i < end_row; ++i) {
 		for (int j = 0; j < image.cols; ++j) {
 			Vec3b pixel = image.at<Vec3b>(i, j);
-			uchar gray = 0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0];
+			uchar gray = 0.3 * pixel[2] + 0.59 * pixel[1] + 0.11 * pixel[0];
 			image.at<Vec3b>(i, j) = Vec3b(gray, gray, gray);
 		}
 	}
 }
+//se utiliza la clase threads para la conversion
 void grayscaleWithThreads(Mat& image, int numThreads) {
 	vector<thread> threads;
 	int rows_per_thread = image.rows / numThreads;
@@ -33,6 +35,7 @@ void grayscaleWithThreads(Mat& image, int numThreads) {
 }
 
 int main() {
+	//se lee la imagen a convertir por consola
 	string originalImagePath, grayscaleImagePath;
 	cout << "Ingrese el nombre del archivo de imagen original: ";
 	cin >> originalImagePath;
@@ -40,19 +43,20 @@ int main() {
 	Mat originalImage = imread(originalImagePath, IMREAD_COLOR);
 
 	int numThreads;
+	//se lee la cantidad de hilos a usar
 	cout << "Ingrese la cantidad de hilos: ";
 	cin >> numThreads;
 
 	Mat grayscaleImage = originalImage.clone();
-
+	//inicia la medicion de tiempo de ejecucion
 	Tiempo_ejec medicion;
 	medicion.iniciarMedicion();
 
 	grayscaleWithThreads(grayscaleImage, numThreads);
 	medicion.finalizarMedicion();
-
+	//muestra el tiempo de ejecucion
 	cout << "Tiempo de ejecucion: " << medicion.obtenerDuracion() << " milisegundos" << endl;
-
+	//solicita el nombre del archivo de salida 
 	cout << "Ingrese el nombre del archivo de imagen en escala de grises: ";
 	cin >> grayscaleImagePath;
 
